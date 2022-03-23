b87913703ff6871833e2431fcf91f18c1dbfcbd6
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-03-23 08:06:18 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1KgHxsEVr3EIqN3hRJ3VHZZ4', {
     expand: ['product']
   })
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
