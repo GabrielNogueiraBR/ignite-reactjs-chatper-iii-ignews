b77a9b4a4297922c700459ae7f9c12b962ff6ad0
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-03-25 06:36:57 -0300

    Saving user email in FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query } from "faunadb";
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -8,6 +12,23 @@ export default NextAuth({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
-    // ...add more providers here
   ],
-})
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.Create(query.Collection("users"), { data: { email } })
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+});
