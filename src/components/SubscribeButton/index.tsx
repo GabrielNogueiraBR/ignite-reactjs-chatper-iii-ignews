3414a2fc12d9423b561e43b1ec268b5c76eb5628
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-07-26 08:32:05 -0300

    chore: adding subscribe function on button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -6,20 +8,34 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
 
-  function handleSubscribe() {
-    if(!session){
-      signIn('github')
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
       return;
     }
 
     // creation of checkout session in stripe
-    
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
