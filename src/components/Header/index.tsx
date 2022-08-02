194fd087702bc7a9911d0fd11008ba7ab9c8400f
Gabriel Nogueira <gabrielnoog7@gmail.com>
2022-08-01 21:37:05 -0300

    chore: updating nav menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,26 @@
-import { SingInButton } from '../SingInButton';
-import styles from './styles.module.scss'
+import { SingInButton } from "../SingInButton";
+import Image from "next/image";
+
+import styles from "./styles.module.scss";
+import { ActiveLink } from "../ActiveLink";
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="logo ig.news" />
+        <Image
+          src="/images/logo.svg"
+          alt="logo ig.news"
+          width={110}
+          height={31}
+        ></Image>
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SingInButton />
